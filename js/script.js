be9c8cafda6b5cb5a14b7fb8dfc84a37846bff18
Ulrik05 <91838842+Ulrik05@users.noreply.github.com>
2022-01-30 07:45:38 +0100

    rest latter

--- js/script.js
+++ js/script.js
@@ -12,3 +12,41 @@ const cats = [
     }
 ];
 
+// Q1
+
+const cat = {
+    complain: function() {
+        console.log("Meow!")
+    }
+};
+
+cat.complain();
+
+// Q2
+
+const heading = document.querySelector("h3");
+
+console.log(heading.innerHTML)
+
+heading.innerHTML = "Updated Heading";
+
+// Q3
+
+heading.style.fontSize = "2em";
+
+// Q4 added class subheading on h3
+
+// Q5
+
+const paragraph = document.querySelectorAll("p");
+
+console.log(paragraph);
+
+for(let i = 0; i < paragraph.length; i++) {
+
+    paragraph[i].style.color = "red";
+
+}
+
+// Q6
+
