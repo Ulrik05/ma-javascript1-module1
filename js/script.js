1f0cab3562ff046311339463c36edfbd268a168e
Ulrik05 <91838842+Ulrik05@users.noreply.github.com>
2022-01-30 23:45:08 +0100

    final
    Q 8

--- js/script.js
+++ js/script.js
@@ -50,3 +50,61 @@ for(let i = 0; i < paragraph.length; i++) {
 
 // Q6
 
+const resultsContainer = document.querySelector(".results");
+
+console.log(resultsContainer.innerHTML)
+
+resultsContainer.innerHTML = "<p>New Paragraph</p>";
+
+resultsContainer.style.background = "yellow";
+
+// Q7
+
+function namesOfCats(list) {    
+
+    for(let i= 0; i < list.length; i++) {
+            console.log(list[i]);
+        }
+    
+    }
+
+    namesOfCats(cats);
+
+// Q8 I couldnt identify the age to be Age Unknown
+
+function createCats(cats) {
+
+    let listcats = "";
+
+    for(let i = 0; i < cats.length; i++) {
+      //  console.log(cats[i]);
+
+        listcats += `<h5>Name: ${cats[i].name}</h5>
+                        <p>Age: ${cats[i].age}</p>`
+        
+           
+             if (cats[i].age = undefined) {
+
+            } 
+                    
+          else  {
+
+            (cats[i].age === "Age Unknown")
+
+          };
+
+    };
+    
+
+    const finalHtml = "<ul>" + listcats + "</ul>";
+
+    return finalHtml;
+
+}
+
+const newHtml = createCats(cats);
+
+createCats(newHtml);
+
+const catContainer = document.querySelector(".cat-container");
+catContainer.innerHTML = newHtml;
